98b3ce3a79dd8563fb53f7b050dde1edcff26d39
Luiz Carlos Santana <112254950+luizcarlossantana@users.noreply.github.com>
2023-07-21 19:08:36 -0300

    começando projeto
    projeto criado com Spring initializr

---
+++ api/src/main/java/com/ClientPro/api/ClientProApplication.java
@@ -0,0 +1,13 @@
+package com.ClientPro.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientProApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/ClientPro/api/ClientProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ClientPro.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
